ebf837ca0b399771992cacff3971b7fb36b7f65f
Dziana Kavaleva <dianakove32@gmail.com>
2021-03-03 20:47:35 +0300

    change nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,10 @@ export default function Navbar() {
     return (
         <ul className="Navbar">
 
-        <Link  className="Navbar-link" to='/'> <li>home</li></Link>
-        <Link className="Navbar-link" to='/weather'> <li> weather</li></Link>
+        {/* <Link  className="Navbar-link" to='/'> <li>home</li></Link> */}
+
         <Link className="Navbar-link" to='/characterList'> <li> chatacterList</li></Link>
+           <Link className="Navbar-link" to='/weather'> <li> weather</li></Link>
         <Link  className="Navbar-link" to='/character'> <li> chatacter </li></Link>
 
         </ul>
